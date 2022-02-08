b6e62c1d7997e49a94eaa519c7f4bb80bf975aef
Pavel <iampetrovpavel@gmail.com>
2022-02-08 15:48:29 +0300

    test deploy client

--- client/pages/index.js
+++ client/pages/index.js
@@ -2,6 +2,7 @@ import Card from '../components/Card.jsx'
 
 
 export default function Home({currentUser}) {
+  console.log("Test deploy")
   return (
     <div className='row'>
       <Card className='col-m-4 col-t-2 col-1'/>
