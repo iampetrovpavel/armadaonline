53c937ff66977a5a097d0a02359ed58d4d27e7ac
Pavel <iampetrovpavel@gmail.com>
2022-02-11 14:28:08 +0300

    Client main page design done FIX

--- client/components/DirectionCart.jsx
+++ client/components/DirectionCart.jsx
@@ -5,7 +5,7 @@ const DirectionCart = ({direction}) => {
     const {name, description, img} = direction
     const [trialForm, showTrialForm] = useState(false)
     return (
-            <div className="col-2" style={{padding: '0em', position: 'relative'}}>
+            <div className="col-t-4 col-2" style={{padding: '0em', position: 'relative'}}>
                 <div className="direction-cart" style={{backgroundImage: `url(${img})`, backgroundSize: 'cover'}}>
                     <div className='inline-block col-2 float-right direction-cart-drawer'
                         style={{height: '100%', padding: '2em', top: trialForm?'-400px':'0'}}>
@@ -16,7 +16,7 @@ const DirectionCart = ({direction}) => {
                         <button className="direction-cart-button" onClick={()=>showTrialForm(true)}>Записаться</button>
                     </div>
                     <div className="direction-cart-trial" style={{top: trialForm?'0px':'400px'}}>
-                        <span onClick={()=>showTrialForm(false)} class="close"/>
+                        <span onClick={()=>showTrialForm(false)} className="close"/>
                         <h3>Ваше имя</h3>
                         <input></input>
                         <h3>Номер телефона</h3>
